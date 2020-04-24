eb13977a067df1faec6fe97c065a8f27b7eda616
Francesco Mistri <francesco.mistri98@gmail.com>
2020-04-24 22:31:56 +0200

    AbstractProcessor.process implementation
    Not yet testable in a full environment(missing AbstractWorld implementation).

--- desper/data/world.py
+++ desper/data/world.py
@@ -1,6 +1,22 @@
 import esper
 
 
+class AbstractComponent:
+    """An inheritance based component for an entity-component design.
+
+    It's designed to be used with an AbstractProcessor.
+    Any AbstractComponent subclass should implement the update method.
+    """
+
+    def update(self, world, *args, **kwargs):
+        """Base method for main game logic.
+
+        This method is called presumably at each frame(at each
+        AbstractProcessor.process call).
+        """
+        raise NotImplementedError
+
+
 class AbstractProcessor(esper.Processor):
     """An inheritance based processor.
 
@@ -11,23 +27,17 @@ class AbstractProcessor(esper.Processor):
     NB: Despite the name, it's not an abstract class(in polymorphic
     terms. While it's designed to be derived, it's not compulsive).
     """
-    def process(world, *args, **kwargs):
-        pass
 
-
-class AbstractComponent:
-    """An inheritance based component for an entity-component design.
-
-    It's designed to be used with an AbstractProcessor.
-    Any AbstractComponent subclass should implement the update method.
-    """
-    def update(world, *args, **kwargs):
+    def process(self, *args, **kwargs):
         """Base method for main game logic.
 
-        This method is called presumably at each frame(at each
-        AbstractProcessor.process call).
+        This method calls AbstractComponent.update on all the
+        AbstractComponents in his bound world. If bound to an
+        AbstractWorld this means calling all the update methods of all
+        the instances deriving from AbstractComponent.
         """
-        raise NotImplementedError
+        for ent, comp in self.world.get_components(AbstractComponent):
+            comp.update(self.world)
 
 
 class AbstractWorld(esper.World):
