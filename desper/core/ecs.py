89efa60704cb6da8295791dc4428faf9dc4d0c58
Francesco Mistri <francesco.mistri98@gmail.com>
2020-09-16 02:06:14 +0200

    Added proper docstrings for CoroutineProcessor

--- desper/core/ecs.py
+++ desper/core/ecs.py
@@ -18,7 +18,64 @@ class _WaitingGenerator:
 
 
 class CoroutineProcessor(esper.Processor):
-    """An ecs processor that manages coroutines."""
+    """An ecs processor that manages coroutines.
+
+    Note that with "coroutines" we **do not** mean python's
+    `builtin coroutines <https://bit.ly/2FHc87u>`_. Builtin coroutines
+    are asynchronous and consequently unstable for games(there might
+    be instances in which python's coroutines can be helpful, but
+    usually not for game logic). Desper coroutines are therefore
+    generators(generator objects, precisely), which can be managed
+    easily by a logic controller(this class).
+    From now on, when "coroutine" is mentioned, "generator" is meant.
+
+    Coroutines can be managed by two main methods, :py:meth:`start`,
+    that will insert and start the coroutine into the system and
+    :py:meth:`kill` that will stop and remove the coroutine from the
+    system.
+    The removed generator is returned, so that it can be started again
+    if wanted by the user(the killed generator is simply removed,
+    meaning that if reinserted with :py:meth:`start` it will resume
+    from where it stopped).
+
+    The coroutine will be called once per frame. To return the control
+    use ``yield``.
+
+    e.g.
+    .. code-block
+
+       def coroutine():
+            # Spawn enemies every 60 frames
+            while True:
+                spawn_enemies()
+                yield 60
+
+        # ...
+        processor.start(coroutine())
+
+    As shown in the example, when yielding a number(int) x the coroutine
+    will be paused for x frames(60 frames in the example), and resumed
+    immediately after.
+
+    To stop a coroutine, use it's generator.
+
+    e.g.
+    .. code-block
+
+       def coroutine():
+           # ...
+
+        # ...
+        gen = coroutine()       # Create generator
+        processor.start(gen)    # Start coroutine
+        # ...
+
+        processor.kill(gen)     # Stop previously started coroutine
+
+    When a coroutine ends(the generator terminates it's execution, when
+    the end of the code is reached) it's automatically removed from
+    execution.
+    """
 
     def __init__(self):
         self._generators = {}
@@ -29,7 +86,14 @@ class CoroutineProcessor(esper.Processor):
         self._timer = 0
 
     def start(self, generator):
-        """Add and start a coroutine, represented by a generator object."""
+        """Add and start a coroutine, represented by a generator object.
+
+        :param generator: The generator object representing the
+                          coroutine.
+        :return: The started generator object.
+        :raises TypeError: If `generator` isn't a generator object.
+        :raises ValueError: If `generator` is already being executed.
+        """
         if not inspect.isgenerator(generator):
             raise TypeError('Only generator objects are accepted')
 
@@ -41,7 +105,18 @@ class CoroutineProcessor(esper.Processor):
         return generator
 
     def kill(self, generator):
-        """Stop and remove a coroutine, given the generator."""
+        """Stop and remove a coroutine, given the generator.
+
+        Paused coroutines can be killed too.
+
+        :param generator: The generator object representing the
+                          coroutine.
+        :return: The killed generator object.
+        :raises TypeError: If `generator` isn't a generator object.
+        :raises ValueError: If `generator` isn't being executed(has
+                            already been killed or terminated it's
+                            execution).
+        """
         if not inspect.isgenerator(generator):
             raise TypeError('Only generator objects are accepted')
 
@@ -60,6 +135,10 @@ class CoroutineProcessor(esper.Processor):
         return generator
 
     def process(self, *args):
+        """Process one frame of all the currently active coroutines.
+
+        And unpause coroutines if necessary.
+        """
         # Manage waiting coroutines
         if len(self._wait_queue) > 0:
             self._timer += 1
