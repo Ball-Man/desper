b44e887c27bf2275f6a076ee152ee16a5ad1e6db
Francesco <francesco.mistri98@gmail.com>
2022-10-01 18:12:47 +0200

    Added global scope default SimpleLoop instance
    desper.core.default_loop.

--- desper/core/__init__.py
+++ desper/core/__init__.py
@@ -2,3 +2,9 @@ from .events import *       # NOQA
 from .model import *        # NOQA
 from .logic import *        # NOQA
 from .loop import *         # NOQA
+
+# Default global loop object.
+# To simplify most common user cases, a loop object is constructed
+# by default and will automatically be used by desper functions if
+# different behaviour is not specified.
+default_loop = SimpleLoop()
