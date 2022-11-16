eb8145550e60878c5590a839bfef053e4daf3eba
Francesco <francesco.mistri98@gmail.com>
2022-11-16 17:53:00 +0100

    Documented default loop and resource map
    Default global instances for ease of use.

--- desper/core/__init__.py
+++ desper/core/__init__.py
@@ -3,8 +3,19 @@ from .model import *        # NOQA
 from .logic import *        # NOQA
 from .loop import *         # NOQA
 
-# Default global loop object.
-# To simplify most common user cases, a loop object is constructed
-# by default and will automatically be used by desper functions if
-# different behaviour is not specified.
 default_loop = SimpleLoop()
+"""Default global loop object.
+
+To simplify most common user cases, a loop object is constructed
+by default and will automatically be used by desper functions if
+different behaviour is not specified.
+"""
+
+resource_map = ResourceMap()
+"""Default resource map container.
+
+To simplify most common use cases, a default global resource map is
+constructed. Users are encouraged to use this instance for global
+project resources. Instancing a custom map is obviously always an
+option, but it is unnecessary in common cases.
+"""
