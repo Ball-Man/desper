a2e69514bb22afb0e45394c4f691bf3b39b0c573
Francesco <francesco.mistri98@gmail.com>
2022-12-27 21:46:09 +0100

    setup.py pipy conformity
    Add minimum Python requirement to setup.py, currently 3.9.
    Remove unsupported classifiers and add long description content type (markdown).

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 README = open('README.md').read()
 
 setup(name='desper',
-      classifiers=['Development Status :: 3 - Beta',
-                   'Intended Audience :: Developers',
+      classifiers=['Intended Audience :: Developers',
                    'Programming Language :: Python :: 3 :: Only',
                    'Topic :: Games/Entertainment'],
+      python_requires='>=3.9',
       version='1.1.0',
       description='A Python3 game development toolkit for resource and logic '
                   'management',
+      long_description_content_type='text/markdown',
       long_description=README,
       url='http://github.com/Ball-Man/desper',
       author='Francesco Mistri',
